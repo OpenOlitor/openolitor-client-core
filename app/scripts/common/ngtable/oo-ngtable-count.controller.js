c6a1090921e2fea610dea308d37672f85e819164
Alwin Egger <alwin.egger@tegonal.com>
2017-01-17 17:53:53 +0100

    OO-473 NgTable-Übersichten: Cookie um die gewählte Anzahl Cols zu speichern

---
+++ app/scripts/common/ngtable/oo-ngtable-count.controller.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/**
+ */
+angular.module('openolitor-core')
+  .controller('NgTableCountController', ['$scope', '$cookies',
+    function($scope, $cookies) {
+
+      var deepFind = function(obj, path) {
+        var paths = path.split('.'),
+          current = obj,
+          i;
+
+        for (i = 0; i < paths.length; ++i) {
+          if (current[paths[i]] === undefined) {
+            return undefined;
+          } else {
+            current = current[paths[i]];
+          }
+        }
+        return current;
+      };
+
+      var setCookie = function(count) {
+        $cookies.put('ngTableCount', count);
+      };
+
+      var getCookie = function() {
+        var r = $cookies.get('ngTableCount');
+        if(angular.isUndefined(r)) {
+          r = 25;
+        }
+        return r;
+      };
+
+      $scope.count = function(count) {
+        deepFind($scope.$parent, 'params').count(count);
+        setCookie(count);
+      };
+
+      $scope.fetchStoredCount = function() {
+        return getCookie();
+      };
+
+      deepFind($scope.$parent, 'params').count($scope.fetchStoredCount());
+    }
+  ]
+);
