cba2c7d7a7b2a662e37d34013c7294d0c8b5f75a
Alwin Egger <alwin.egger@tegonal.com>
2017-10-10 14:01:42 +0200

    OO-770: Added o-sliding-detail directive

---
+++ app/scripts/common/components/oo-sliding-detail.directive.js
@@ -0,0 +1,44 @@
+'use strict';
+
+angular.module('openolitor-core').directive('ooSlidingDetail',
+  function() {
+    return {
+      restrict: 'E',
+      replace: true,
+      scope: {
+        slideIn: '&',
+        slideOutFunction: '=?'
+      },
+      transclude: true,
+      templateUrl: 'scripts/common/components/oo-sliding-detail.directive.html',
+      controller: function($scope) {
+
+        $scope.$watch($scope.slideIn,
+          function(res) {
+            if (res) {
+              $scope.openNav();
+            } else {
+              $scope.closeNav();
+            }
+          });
+
+        /* Set the width of the side navigation to 250px */
+        $scope.openNav = function() {
+          $scope.stateStyle = {width: '400px',right:'0'};
+        };
+
+        /* Set the width of the side navigation to 0 */
+        $scope.closeNav = function() {
+          $scope.stateStyle = {width: '0',right:'-20px'};
+        };
+
+        $scope.callFctSlideOut = function() {
+          if(angular.isDefined($scope.slideOutFunction) && typeof $scope.slideOutFunction === 'function') {
+            $scope.slideOutFunction();
+          } else {
+            $scope.closeNav();
+          }
+        };
+      }
+    };
+  });
