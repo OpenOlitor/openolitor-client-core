72974f872d76a8f1bc91c30e3541201ca0202272
Mikel Cordovilla <mikel.cordovilla@tegonal.com>
2022-02-23 11:22:54 +0100

    #362 add reply to default object

--- app/scripts/common/components/oo-email-dialog.controller.js
+++ app/scripts/common/components/oo-email-dialog.controller.js
@@ -19,7 +19,7 @@ angular.module('openolitor-core')
 
       $scope.commandIssued = false;
 
-      $scope.defaultEmptyTemplate = {id:0,label:gettext('Keine Vorlage'),subject:undefined,body:undefined,attachInvoice:false};
+      $scope.defaultEmptyTemplate = {id:0,label:gettext('Keine Vorlage'),subject:undefined,body:undefined,attachInvoice:false, replyTo:undefined};
       $scope.templateT = [$scope.defaultEmptyTemplate];
       MailerService.getTemplates().then(function(result){
           angular.forEach(result.data, function(value){
