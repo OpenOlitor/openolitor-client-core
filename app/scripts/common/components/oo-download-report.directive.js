9a9d61d545676af225289d4cdc743bd513b2ca3f
Mikel Cordovilla <mikel.cordovilla@tegonal.com>
2022-03-23 16:42:47 +0100

    OO#80 adding a menu to select pdf merged or zip when downloading invoices

---
+++ app/scripts/common/components/oo-download-report.directive.js
@@ -0,0 +1,51 @@
+'use strict';
+
+angular.module('openolitor-core').directive('ooDownloadReport', function() {
+  return {
+    restrict: 'E',
+    replace: true,
+    scope: {
+      postPath: '=',
+      onDownloaded: '&',
+      onClose: '&',
+      defaultFileName: '=',
+      ids: '=?',
+      form: '=?'
+    },
+    templateUrl: 'scripts/common/components/oo-download-report.directive.html',
+    controller: function(
+      $scope,
+      $http,
+      appConfig,
+      FileUtil,
+      gettext,
+      lodash,
+      alertService,
+      PDF_DOWNLOAD_TYPES,
+      EnumUtil
+    ) {
+      $scope.pdfDownloadTypes = EnumUtil.asArray(PDF_DOWNLOAD_TYPES); 
+
+      $scope.form = {
+        pdfMerge: 'pdfMerge'
+      };
+
+      $scope.download = function() {
+        $scope.error = undefined;
+        FileUtil.downloadPost('rechnungen/aktionen/downloadrechnungen',{
+          'ids': $scope.ids,
+          'pdfMerge': $scope.form.pdfMerge
+        });
+      };
+    },
+
+    link: function($scope, $elem, $attrs) {
+      $scope.$on('resetDirectiveDownloadReport', function(event) {
+        var form = document.querySelector('[name=downloadForm]');
+        $scope.form = {
+          pdfMerge: $scope.form.pdfMerge
+        };
+      });
+    }
+  };
+});
